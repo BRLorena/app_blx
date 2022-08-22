995fe70d0c9c8520f79ec4e6cac642795ea49a8d
BRLorena <brunomlorena95@gmail.com>
2022-08-22 20:23:37 +0100

    Add rota POST,  GET -listProdutos e criado a db. Atencao que agora estou trabalhando a partir do dir do projeto

--- src/infra/sqlalchemy/repositorios/produto.py
+++ src/infra/sqlalchemy/repositorios/produto.py
@@ -1,29 +1,31 @@
 from sqlalchemy.orm import Session
-from src.infra.sqlalchemy.models import models
-from src.schemas import schemas
+from infra.sqlalchemy.models import models
+from schemas import schemas
 
 
-class RepositorioProduto():
+class RepositorioProduto:
+    def __init__(self, db: Session):
+        self.db = db
 
-  def __init__(self, db:Session):
-    self.db = db
+    # chamei a class produto P/ converter schema em modelo P/ o DB
+    def criar(self, produto: schemas.Produto):
+        db_produto = models.Produto(
+            nome=produto.nome,
+            detalhes=produto.detalhes,
+            preco=produto.preco,
+            disponivel=produto.disponivel,
+        )
+        self.db.add(db_produto)  # add ao bd
+        self.db.commit()  # confirma
+        self.db.refresh(db_produto)  # atualiza
+        return db_produto  # retorna os dados
 
-# chamei a class produto P/ converter schema em modelo P/ o DB
-  def criar(self, produto: schemas.Produto):
-    db_produto = models.Produto(nome=produto.nome,
-    detalhes = produto.detalhes, preco = produto.preco,
-    disponivel = produto.disponivel )
-    self.db.add(db_produto) # add ao bd
-    self.db.commit() # confirma 
-    self.db.refresh(db_produto) # atualiza
-    return db_produto #retorna os dados
+    def listar(self):
+        produtos = self.db.query(models.Produto).all()
+        return produtos
 
-  def listar(self):
-    produtos = self.db.query(models.Produto).all()
-    return produtos
+    def obter(self):
+        pass
 
-  def obter(self):
-    pass
-
-  def remover(self):
-    pass
+    def remover(self):
+        pass
--- src/server.py
+++ src/server.py
@@ -1,51 +1,68 @@
-
 from uuid import uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware  # permitir front usar API
+from schemas.schemas import Produto  # import class Produto from schemas
+from infra.sqlalchemy.repositorios.produto import RepositorioProduto
+from sqlalchemy.orm import Session
+from infra.sqlalchemy.config.database import get_db, criar_bd
 
+# Criar bd
+criar_bd()
+
+# start APP
 app = FastAPI()
 
-#permissions
-origins = ['http://127.0.0.1:5500']
+# permissions
+# origins = ["http://127.0.0.1:5500"]
 
-#cadastrar os interceptors (middleware) permite CORS p/ algumas requests
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
-)
+# # cadastrar os interceptors (middleware) permite CORS p/ algumas requests
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
-#common
-@app.get("/animais")
-async def list_animals():
-  return db
+@app.post("/produtos")  # Constructor RepositorioProduto precisa de um db
+def criar_produtos(produto: Produto, db: Session = Depends(get_db)):
+    produto_criado = RepositorioProduto(db).criar(produto)
+    return produto_criado
 
-@app.get("/animal/{id_animal}")
-async def getAnimal(id_animal: str):
-  for animal in db:
-    if animal.id == id_animal:   
-      return {'mensagem': f'O id do {animal.name} é {id_animal}' }
-  return {'mensagem': 'Animal not found'}
 
-@app.post("/animal")
-async def create_animal(animal: Animal):
-  animal.id = str(uuid4()) #gerar id automatico -> cast to str
-  db.append(animal)
-  return {'Animal ': f' {animal.name} cadastrado com sucesso' }
+## common
+@app.get("/produtos")
+def listar_produtos(db: Session = Depends(get_db)):
+    produtos = RepositorioProduto(db).listar()
+    return produtos
 
-@app.delete("/animal/{id_animal}")
-async def remove_animal(id_animal: str):
-  posicao_animal = -1
-  for i, animal in enumerate(db):
-    if animal.id == id_animal:
-      posicao_animal = i
-      break #encontrou posso parar
-  if posicao_animal != -1:
-    db.pop(posicao_animal)
-    return {'mensagem': 'Animal removido com sucesso'}
-  else:  
-    return {'mensagem ': f'Animal não localizado' }
+
+# @app.get("/animal/{id_animal}")
+# async def getAnimal(id_animal: str):
+#     for animal in db:
+#         if animal.id == id_animal:
+#             return {"mensagem": f"O id do {animal.name} é {id_animal}"}
+#     return {"mensagem": "Animal not found"}
+
+
+# @app.post("/animal")
+# async def create_animal(animal: Animal):
+#     animal.id = str(uuid4())  # gerar id automatico -> cast to str
+#     db.append(animal)
+#     return {"Animal ": f" {animal.name} cadastrado com sucesso"}
+
+
+# @app.delete("/animal/{id_animal}")
+# async def remove_animal(id_animal: str):
+#     posicao_animal = -1
+#     for i, animal in enumerate(db):
+#         if animal.id == id_animal:
+#             posicao_animal = i
+#             break  # encontrou posso parar
+#     if posicao_animal != -1:
+#         db.pop(posicao_animal)
+#         return {"mensagem": "Animal removido com sucesso"}
+#     else:
+#         return {"mensagem ": f"Animal não localizado"}
